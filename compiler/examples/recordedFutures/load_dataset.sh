467d07b50e71d2dffe65fc6a9036583864a7e8d7
Ameya <ameyap@umd.edu>
2020-09-26 16:57:15 -0400

    Script to load csv into psql database

---
+++ compiler/examples/recordedFutures/load_dataset.sh
@@ -0,0 +1,6 @@
+docker cp ./maryland_coords.csv kyrix_db_1:/maryland_coords.csv         # copy the csv file to the db container
+docker exec -it kyrix_db_1 psql postgresql://postgres:kyrixftw@localhost/postgres -c "CREATE DATABASE recFut;"      # create database in the db container
+#docker exec -it kyrix_db_1 psql postgresql://postgres:kyrixftw@localhost/postgres -c "USE recfut;"      # connect to recFut database
+docker exec -it kyrix_db_1 psql postgresql://postgres:kyrixftw@localhost/recfut -c "DROP TABLE IF EXISTS Facilities;"    # drop table if previously existed
+docker exec -it kyrix_db_1 psql postgresql://postgres:kyrixftw@localhost/recfut -c "CREATE TABLE Facilities(sid SERIAL, rfId TEXT, type TEXT, name TEXT, lat FLOAT8, lng FLOAT8, PRIMARY KEY(sid));"      # create Facilities table in recFut database
+docker exec -it kyrix_db_1 psql postgresql://postgres:kyrixftw@localhost/recfut -c "COPY Facilities(sid, rfId, type, name, lat, lng) FROM '/maryland_coords.csv' DELIMITER ',' CSV HEADER;"      # load data in Facilities table
