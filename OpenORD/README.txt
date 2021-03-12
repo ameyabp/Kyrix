82bc166e5ca6224e99ab94015e713af822cde165
Ishan Sen <ishans1999@gmail.com>
2021-03-12 23:04:16 +0100

    modified indexer to call layout script, modified dockerfile to add make and compiler, TODO: update dockerfile to automatically load OpenORD layout into container instead of using a manual script

---
+++ OpenORD/README.txt
@@ -0,0 +1,1 @@
+Run the load script only once per docker build. If the container is rebuilt, run it again to load the layout scripts into the container.
--- compiler/examples/template/app.js
+++ compiler/examples/template/app.js
@@ -11,3 +11,35 @@ const transforms = require("./transforms");
 const placements = require("./placements");
 
 // definition of projects, views, canvases, layers and jumps
+
+var width = 960 * 2;
+var height = 500 * 2;
+
+// construct a project
+var project = new Project("testing", "../../../config.txt");
+project.addRenderingParams(renderers.renderingParams);
+project.addStyles("../nba/nba.css");
+
+// ================== canvas ===================
+var testingCanvas = new Canvas("canvas", width, height);
+project.addCanvas(testingCanvas);
+
+// layer
+var testingLayer = new Layer(transforms.emptyTransform, false);
+testingCanvas.addLayer(testingLayer);
+//stateBoundaryLayer.addRenderingFunc(renderers.test);
+testingLayer.addRenderingFunc(renderers.test);
+testingLayer.addPlacement({
+    centroid_x: "full",
+    centroid_y: "full",
+    width: "full",
+    height: "full"
+});
+
+var view = new View("testing", 960*2, 500*2);
+project.addView(view);
+project.setInitialStates(view, testingCanvas, 0, 0);
+
+
+
+project.saveProject();
