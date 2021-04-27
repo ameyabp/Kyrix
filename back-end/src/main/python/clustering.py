8bba4f983e074f1a590607d5542e93f01e00ef91
Ishan Sen <ishans1999@gmail.com>
2021-04-27 12:39:52 +0200

    updated as per review

--- back-end/src/main/python/clustering.py
+++ back-end/src/main/python/clustering.py
@@ -6,8 +6,10 @@ import numpy as np
 import pandas as pd
 import sys
 
+
+# sample call: python3 clustering.py authorship ../../../../OpenORD/graphNodesData_level_0.csv ../../../../OpenORD/graphEdgesData_level_0.csv openORD kmeans 1000,500,50 memberNodeCount,paperCount 0.9,0.3 authorName 0
+
 if __name__ == "__main__":
-    print('bla')
     if len(sys.argv) < 11:
         # default 
         print("Not enough arguments")
@@ -37,7 +39,7 @@ if __name__ == "__main__":
         # get id that edges use for source and target labels
         edgeDataID = sys.argv[9] # e.g. authorName
 
-        # boolean for whether graph is directed or not, maybe not needed?
+        # boolean for whether graph is directed or not, 1 if directed, 0 if not
         directed = sys.argv[10]
 
         
@@ -45,22 +47,21 @@ if __name__ == "__main__":
 
     # read in layout of nodes and edges from layout algorithm TODO: MODIFY FOR MORE ALGORITHMS
     layoutNodes = pd.read_csv('../../../../OpenORD/OpenOrd-master/examples/recursive/authorship.coord', names = ['id', 'x', 'y'], sep='\t', header=None)
-    layoutEdges = pd.read_csv('../../../../OpenORD/OpenOrd-master/examples/recursive/authorship.edges', names = ['source', 'target', 'weight'], dtype={'source': str, 'target': str, 'weight': float}, sep='\t', header=None)
 
     # read in user input nodes and edges and infer column types
     inputNodes = pd.read_csv(nodesDir, na_values=[''])
     inputEdges = pd.read_csv(edgesDir, na_values=[''])
 
-
     # create unique edge id using ordering of 'source' and 'target' nodes 
     # where the smaller node id comes first and then target id comes next
     def createUniqueEdgeID(x,y):
+        if directed == 1: # takes in x,y as source,target so return edge id as such
+            return x + '_0_' + y
+
         small = x if x < y else y
         big = y if y > x else x
-        return small + '_0_' + big
+        return small + '_0_' + big # if they are both equal, result is y_0_x 
 
-
-    ### work on setting up layoutEdges df
 
     ### find and remove duplicate edges; may need to aggregate over them? 
     ### this might be an unnecessary step if duplicate edges are intentional
@@ -69,40 +70,21 @@ if __name__ == "__main__":
     visitedEdges = set()
 
     def findDuplicate(x, y):
-        one = x + "_0_" + y
-        two = y + '_0_' + x
+        if directed == 1:
+            one = x + '_0_' + y
+            two = x + '_0_' + y
+        else:
+            one = x + "_0_" + y
+            two = y + '_0_' + x
         if one in visitedEdges or two in visitedEdges:
             return True
         visitedEdges.add(one)
         return False
 
-    layoutEdges['duplicate'] = [findDuplicate(x, y) for x, y in zip(layoutEdges['source'], layoutEdges['target'])]
-
-    layoutEdges = layoutEdges[layoutEdges['duplicate'] == False]
-
-    layoutEdges = layoutEdges.drop(columns=['duplicate'])
-
-    ### end duplicate removal
-
 
     ### re-id the source and target in layout output to ids used in user input, 
     ### important for creating unique edge ids
-
     edgeDataID_to_id = dict(zip(inputNodes['id'].astype(str), inputNodes[edgeDataID]))
-
-    layoutEdges['temp_source'] = [edgeDataID_to_id[source] for source in layoutEdges['source']]
-    layoutEdges['temp_target'] = [edgeDataID_to_id[target] for target in layoutEdges['target']]
-
-    # create unique edge id for layout edges
-    layoutEdges['source_target'] = [createUniqueEdgeID(x, y) for x, y in zip(layoutEdges['temp_source'], layoutEdges['temp_target'])]
-
-    # drop unnecessary columns, these will be found again in the join 
-    layoutEdges = layoutEdges.drop(columns = ['source', 'target', 'temp_source', 'temp_target'])
-
-
-    ### end re-id
-
-    ###layoutEdges df is done being modified now
 
 
     ### work on creating finalEdges df for finalized lowest level edges
@@ -111,7 +93,7 @@ if __name__ == "__main__":
     inputEdges['source_target'] = [createUniqueEdgeID(x, y) for x, y in zip(inputEdges['source'], inputEdges['target'])]
 
     # merge edges on unique edge id
-    finalEdges = pd.merge(layoutEdges, inputEdges, on="source_target")
+    finalEdges = inputEdges
 
     ### done with finalEdges for now
 
@@ -132,6 +114,7 @@ if __name__ == "__main__":
     # set x and y coordinates in edges df
     finalEdges['x1'] = [getX(x) for x in finalEdges['source']]
     finalEdges['y1'] = [getY(x) for x in finalEdges['source']]
+
     finalEdges['x2'] = [getX(x) for x in finalEdges['target']]
     finalEdges['y2'] = [getY(x) for x in finalEdges['target']]
 
@@ -141,3 +124,5 @@ if __name__ == "__main__":
     finalNodes['clusterNodeID'] = finalNodes.index
 
     ### done preparing finalNodes df
+
+    #print(finalEdges.head(5))
